df0d6dd7314b0c4876228dfa98017ecead1ed38c
thekimcreates <joshuayulkim111014@gmail.com>
2026-07-20 20:19:46 -0700

    Update admin-login.js

--- js/admin-login.js
+++ js/admin-login.js
@@ -1,54 +1,140 @@
-import { auth } from "./firebase.js";
+// admin-login.js
 
 
-import { 
-signInWithEmailAndPassword 
-} from 
+import {
+
+auth
+
+}
+
+from "./firebase.js";
+
+
+
+import {
+
+signInWithEmailAndPassword
+
+}
+
+from
+
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 
-const loginForm =
-document.getElementById("loginForm");
 
 
-loginForm.addEventListener(
-"submit",
-async(e)=>{
 
-e.preventDefault();
+
+
+
+const emailInput =
+
+document.getElementById(
+
+"email"
+
+);
+
+
+
+const passwordInput =
+
+document.getElementById(
+
+"password"
+
+);
+
+
+
+const loginButton =
+
+document.getElementById(
+
+"loginButton"
+
+);
+
+
+
+const errorMessage =
+
+document.getElementById(
+
+"error"
+
+);
+
+
+
+
+
+
+
+
+loginButton.onclick = async ()=>{
+
 
 
 const email =
-document.getElementById("email").value;
+
+emailInput.value;
+
 
 
 const password =
-document.getElementById("password").value;
+
+passwordInput.value;
+
+
+
 
 
 try{
 
 
+
 await signInWithEmailAndPassword(
+
 auth,
+
 email,
+
 password
+
 );
 
 
-window.location.href=
+
+
+
+window.location.href =
+
 "dashboard.html";
+
+
+
 
 
 }
 
 catch(error){
 
-alert(
-"Login failed"
-);
+
+
+console.error(error);
+
+
+
+errorMessage.textContent =
+
+"Incorrect email or password.";
+
+
 
 }
 
 
-});
+
+};
